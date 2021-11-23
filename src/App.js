2b8d7a6bde14ba3de8c29c9e1f408c8336a024a9
Lucas Carvalho Punchirolli <lucas.carvalho-punchirolli@osedea.com>
2021-11-23 16:12:14 -0500

    Testing auto deploy

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Leur infra n'est pas bonne
         </a>
       </header>
     </div>
